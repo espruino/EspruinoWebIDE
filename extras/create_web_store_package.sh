814cf2ab4300318a25271491f250c28fcedb9972
Gordon Williams <gw@pur3.co.uk>
2013-11-14 14:37:20 +0000

    use different icon for unpacked web ide

--- extras/create_web_store_package.sh
+++ extras/create_web_store_package.sh
@@ -3,4 +3,10 @@ cd `dirname $0`
 cd ..
 rm -f EspruinoSerialTerminal.zip
 find . -name "*~" -delete
+
+mkdir img/bak
+cp img/icon_*.png img/bak
+cp extras/web_store/icon_*.png img
 zip EspruinoSerialTerminal.zip main.html manifest.json css/*.css css/ui-lightness/*.css css/ui-lightness/images/* img/*.png js/*.js blockly/* blockly/media/*
+cp img/bak/icon_*.png img
+rm -rf img/bak
