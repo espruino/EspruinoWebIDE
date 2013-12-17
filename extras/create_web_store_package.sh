8965758032906eafc8155cc2ae94ca5de4c56bfa
Gordon Williams <gw@pur3.co.uk>
2013-12-17 15:33:36 +0000

    better packaging

--- extras/create_web_store_package.sh
+++ extras/create_web_store_package.sh
@@ -2,11 +2,10 @@
 cd `dirname $0`
 cd ..
 rm -f EspruinoSerialTerminal.zip
-find . -name "*~" -delete
 
 mkdir img/bak
 cp img/icon_*.png img/bak
 cp extras/web_store/icon_*.png img
-zip EspruinoSerialTerminal.zip main.html manifest.json css/*.css css/ui-lightness/*.css css/ui-lightness/images/* img/*.png js/*.js blockly/* blockly/media/*
+zip -9 -r --exclude=@extras/chrome_exclude.lst EspruinoSerialTerminal.zip .
 cp img/bak/icon_*.png img
 rm -rf img/bak
