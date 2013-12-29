b46b6e9ad18c83437d66c3c7830125c7dfa2a317
Gordon Williams <gw@pur3.co.uk>
2013-12-29 18:54:28 +0000

    serial/flasher tweaks to try and make flashing faster/more reliable

--- js/espruino_serial.js
+++ js/espruino_serial.js
@@ -139,7 +139,7 @@ Author: Gordon Williams (gw@pur3.co.uk)
     else
       writeData += data;    
     
-    var blockSize = 32;
+    var blockSize = 60;
 
     showStatus &= writeData.length>blockSize;
     if (showStatus) {
@@ -172,7 +172,7 @@ Author: Gordon Williams (gw@pur3.co.uk)
       sender(); // send data instantly
       // if there was any more left, do it after a delay
       if (writeData!=undefined) {
-        writeInterval = setInterval(sender, 50);
+        writeInterval = setInterval(sender, 120);
       } else {
         if (showStatus)
           Espruino.Status.setStatus("Sent");
