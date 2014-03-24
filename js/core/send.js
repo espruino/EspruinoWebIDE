b47330dc02daca26df7812a386430f26054e3052
Gordon Williams <gw@pur3.co.uk>
2014-03-24 09:47:38 +0000

    fix regression

--- js/core/send.js
+++ js/core/send.js
@@ -24,13 +24,11 @@
         position: "top"
       }, 
       click: function() {
-        Espruino.Core.Terminal.focus(); // give the terminal focus
-        Espruino.callProcessor("sending");
-        if (Espruino.Core.Serial.isConnected()) {
+        Espruino.Core.MenuPortSelector.ensureConnected(function() {
+          Espruino.Core.Terminal.focus(); // give the terminal focus
+          Espruino.callProcessor("sending");
           Espruino.Core.Code.getEspruinoCode(Espruino.Core.CodeWriter.writeToEspruino);
-        } else { 
-          Espruino.Core.Notifications.error("Not Connected");
-        }
+        });
       }
     });
     
