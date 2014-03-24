12a79dc2ed5c753433c915ad4fa4e34edf3f7879
mattbrailsford <me@mattbrailsford.com>
2014-03-24 19:51:28 +0000

    Chnaged icon check to actually remove the update icon on disconnect

--- js/plugins/versionChecker.js
+++ js/plugins/versionChecker.js
@@ -38,6 +38,11 @@
 
       callback(env);
     }); 
+
+    Espruino.addProcessor("disconnected", function(env, callback) {
+      var icon = Espruino.Core.App.findIcon("update");
+      if(icon) icon.remove();
+    });
   }
   
   function checkEnv(env) {
@@ -59,25 +64,20 @@
       if (vAvailable > vCurrent && env.BOARD=="ESPRUINOBOARD") {
         console.log("New Firmware "+tAvailable+" available");
 
-        var icon = Espruino.Core.App.findIcon("update");
-        if(!icon){
-
-          Espruino.Core.App.addIcon({
-            id:'update',
-            icon: 'alert',
-            title: 'New Firmware '+ tAvailable +' available. Click to update.',
-            order: 999,
-            cssClass: 'title-bar__button--alert',
-            area: {
-              name: "titlebar",
-              position: "right"
-            },
-            click: function(){
-              Espruino.Core.MenuSettings.show("Flasher");
-            }
-          });
-
-        }
+        Espruino.Core.App.addIcon({
+          id:'update',
+          icon: 'alert',
+          title: 'New Firmware '+ tAvailable +' available. Click to update.',
+          order: 999,
+          cssClass: 'title-bar__button--alert',
+          area: {
+            name: "titlebar",
+            position: "right"
+          },
+          click: function(){
+            Espruino.Core.MenuSettings.show("Flasher");
+          }
+        });
 
         //Espruino.Core.Notifications.info('New Firmware '+ tAvailable +' available. <a class="flash_menu" style="cursor:pointer">Click here to update</a>');
         //$(".flash_menu").click(function() {
