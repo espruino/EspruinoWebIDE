388339bbc00656c9979ca0621ae8a2b9a5b537a4
Gordon Williams <gw@pur3.co.uk>
2014-03-25 16:07:37 +0000

    Add URL handler for URLS like http://www.espruino.com/webide?code=print(%22Hello%20World%22). Some way towards helping with #55

--- js/background.js
+++ js/background.js
@@ -1,10 +1,23 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  	chrome.app.window.create('main.html', 
-  	{
-  		id:"espruino_mainwindow", 
-  		width: 1024, 
-  		height: 600, 
-  		singleton: true,
-  		frame: 'none'
-  	});
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+  chrome.app.window.create('main.html', {
+    id:"espruino_mainwindow", 
+    width: 1024, 
+    height: 600, 
+    singleton: true,
+    frame: 'none'
+  }, function(win) {
+    if (launchData.id) {
+      // We are called to handle a URL that matches one of our url_handlers.
+      if (launchData.id === 'espruino_code') {
+        if (win.contentWindow.Espruino.Plugins.URLHandler)
+          win.contentWindow.Espruino.Plugins.URLHandler.handle(launchData.url);
+      } else {
+        console.error("Unexpected URL handler ID: " + launchData.id);
+      }
+    }
+    
+  });
+  
+  
+ 
 });
---
+++ js/plugins/urlHandler.js
@@ -0,0 +1,47 @@
+/**
+ Copyright 2014 Gordon Williams (gw@pur3.co.uk)
+
+ This Source Code is subject to the terms of the Mozilla Public
+ License, v2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ 
+ ------------------------------------------------------------------
+  Handle URLS of the form http://www.espruino.com/webide?...
+  These are sent from background.js and are picked up by the Web IDE
+ ------------------------------------------------------------------
+**/
+"use strict";
+(function(){
+  
+  function init() {
+  }
+  
+  function handleQuery(key, val) {
+    if (key=="code")
+      Espruino.Core.EditorJavaScript.setCode(val);
+  }
+  
+  function handle(url) {    
+    console.log("Handling URL "+JSON.stringify(url));
+    url = (url);
+    var q = url.indexOf("?");
+    if (q<0) return;
+    var query = url.substr(q+1).split("&");
+    for (var i in query) {
+      var eq = query[i].split("=");
+      if (eq.length==1)
+        handleQuery(eq[0],undefined);
+      else if (eq.length==2)
+        handleQuery(decodeURIComponent(eq[0]),decodeURIComponent(eq[1]));
+      else
+        console.warn("Didn't understand query section "+JSON.stringify(query[i]));
+    }
+  }
+    
+
+  Espruino.Plugins.URLHandler = {
+    init : init,
+    
+    handle : handle, // handle a URL
+  };
+}());
