8d83e04c8972d9922ecadfaf5e7bc53fac317bd7
mattbrailsford <me@mattbrailsford.com>
2014-04-02 08:58:45 +0100

    Added version number to about panel

--- js/core/settingsAbout.js
+++ js/core/settingsAbout.js
@@ -12,22 +12,40 @@
 "use strict";
 (function(){
   
+  function getVersion(callback) 
+  {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', 'manifest.json');
+    xmlhttp.onload = function (e) {
+        var manifest = JSON.parse(xmlhttp.responseText);
+        callback(manifest.version);
+    }
+    xmlhttp.send(null);
+  }
+
   function init() {
-    Espruino.Core.Config.addSection("About", {
-      description : "About the Espruino Web IDE",
-      sortOrder : -1000,
-      getHTML : function(callback) {      
-        $.get("/data/settings_about.html", function(data) {
-          callback(data);
-          var html;
-          if (Object.keys(Espruino.Core.Env.getBoardData()).length > 0)
-            html = Espruino.Core.Utils.htmlTable(Espruino.Core.Env.getBoardData());
-          else
-            html = "<p>Unable to get board information</p>";
-          $('.board_info').html( html );
-        });
-      }
+
+    getVersion(function(version){
+
+      Espruino.Core.Config.addSection("About", {
+        description : "About the Espruino Web IDE v"+ version,
+        sortOrder : -1000,
+        getHTML : function(callback) {      
+          $.get("/data/settings_about.html", function(data) {
+            callback(data);
+            var html;
+            if (Object.keys(Espruino.Core.Env.getBoardData()).length > 0)
+              html = Espruino.Core.Utils.htmlTable(Espruino.Core.Env.getBoardData());
+            else
+              html = "<p>Unable to get board information</p>";
+            $('.board_info').html( html );
+          });
+        }
+      });
+
     });
+
+    
   }
   
   Espruino.Core.SettingsAbout = {
