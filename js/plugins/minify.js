8bdd10715c5c3b016a19a6e46a191f491bc0ee2d
Gordon Williams <gw@pur3.co.uk>
2014-04-09 11:33:15 +0100

    rpeort minify errors - fix #65

--- js/plugins/minify.js
+++ js/plugins/minify.js
@@ -19,7 +19,10 @@
       section : "Communications",
       name : "Minification",
       description : "Automatically minify sent code? This will save Espruino's memory and may increase execution speed, but it will make debugging harder.",
-      type : {"":"No Minification","WHITESPACE_ONLY":"Whitespace Only","SIMPLE_OPTIMIZATIONS":"Simple Optimizations","ADVANCED_OPTIMIZATIONS":"Advanced Optimizations"},
+      type : { "":"No Minification",
+               "WHITESPACE_ONLY":"Whitespace Only",
+               "SIMPLE_OPTIMIZATIONS":"Simple Optimizations",
+               "ADVANCED_OPTIMIZATIONS":"Advanced Optimizations (not recommended)"},
       defaultValue : ""
     });
     
@@ -27,28 +30,54 @@
     Espruino.addProcessor("transformForEspruino", minifyCode);
   }
   
+  function closureCompiler(code, output_info, callback) {
+    var minifyObj = $.param({
+      compilation_level: Espruino.Config.MINIFICATION_LEVEL,
+      output_format: "text",
+      output_info: output_info,
+      js_code: code
+    });      
+    $.post(minifyUrl, minifyObj, function(minifiedCode) {      
+      code = minifiedCode;          
+    },"text")
+      .error(function() { 
+        Espruino.Core.Notifications.error("HTTP error while minifying.");
+      })
+      .complete(function() {
+        // ensure we call the callback even if minification failes
+        callback(code);
+    });
+  }
+
+  function removeBinaryNumbers(code) {
+    // replace all binary numbers with parseInt
+    return code.replace(/0b([01][01]*)/g, "parseInt(\"$1\",2)");
+  }
+
   function minifyCode(code, callback) {
     if (Espruino.Config.MINIFICATION_LEVEL != "") {
-      var minifyObj = $.param({
-        compilation_level:Espruino.Config.MINIFICATION_LEVEL,
-        output_format:"text",
-        output_info:"compiled_code",
-        js_code:code
-      });      
-      $.post(minifyUrl, minifyObj, function(minifiedCode){
-        console.log("Minification complete. Code Size reduced from " + code.length + " to " + minifiedCode.length);
-        code = minifiedCode;          
-      },"text")
-        .error(function() { 
-          console.warn("Error minifying..."); 
-        })
-        .complete(function() {
-          // ensure we call the callback even if minification failes
+      // if we've been asked to minify...       
+      closureCompiler(removeBinaryNumbers(code), 'compiled_code', function(minified) {
+        if (minified.trim()!="") { 
+          console.log("Minification complete. Code Size reduced from " + code.length + " to " + minified.length);
+          console.log(JSON.stringify(minified));
+          callback(minified);
+        } else {
+          Espruino.Core.Notifications.warning("Errors while minifying - sending unminified code.");
           callback(code);
+          // get errors...
+          closureCompiler(code, 'errors', function(errors) {
+            console.log("Closure compiler errors: "+errors);
+            errors.split("\n").forEach(function (err) {
+              if (err.trim()!="")
+                Espruino.Core.Notifications.error(err.trim());
+            });
+          });
+        }
       });
     } else {
-     // just shortcut
-     callback(code);
+      // just pass code onwards
+      callback(code);
     }
   }
   
