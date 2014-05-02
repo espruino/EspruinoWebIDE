8f94ffc4b077027ca39855e854ab2486faa06de6
Gordon Williams <gw@pur3.co.uk>
2014-05-02 08:22:38 +0100

    red flasher warning text

--- js/core/settingsFlasher.js
+++ js/core/settingsFlasher.js
@@ -22,8 +22,10 @@
 
           // Set up warning
           var chromeVer = navigator.userAgent.replace(/.*Chrome\/([0-9]*).*/,"$1");
-          if (chromeVer < 31) {
-            $(".flash_info").css("color","red").html("Your Chrome version is "+chromeVer+". Please upgrade it before trying to flash your Espruino board.");
+          if (chromeVer < 32) {
+            // 32 should work, but if they're going to upgrade let's suggest 33 :)
+            var info = "Your Chrome version is "+chromeVer+". Please upgrade it to 33 or newer before trying to flash your Espruino board."
+            $(".flash_info").css("color","red").html(info);
           }     
           
           // Start button
