6393c7281e182e02e80f754479bb0d1baa5ecc01
Gordon Williams <gw@pur3.co.uk>
2014-05-19 16:15:26 +0100

    improved assembler with labels

--- js/plugins/assembler.js
+++ js/plugins/assembler.js
@@ -18,7 +18,8 @@
     ARM reference
     https://web.eecs.umich.edu/~prabal/teaching/eecs373-f11/readings/ARMv7-M_ARM.pdf
 */ 
-  
+
+  // list of registers (for push/pop type commands) 
   function rlist_lr(value) {
    var regs = value.split(",");
    var vals = { r0:1,r1:2,r2:4,r3:8,r4:16,r5:32,r6:64,r7:128,lr:256 };
@@ -30,7 +31,6 @@
    }
    return bits;
   }
-   
    
   function reg(reg_offset) {
     return function(reg) {
@@ -58,67 +58,11 @@
      return reg(base_offset)(parms[0]) | reg(offset_offset)(parms[0]);
    };
   }
-   
-  function uint7_shr2(offset) {
-    return function(value) {
-      var bits = parseInt(value);
-      if (bits>=0 && bits<=511 && (bits&3)==0)
-        return (bits>>2)<<offset;
-      throw "Invalid number '"+value+"' - must be between 0 and 508 and a multiple of 4";
-    };
-  }
-   
-  function uint8_shr2(offset) {
-    return function(value) {
-      var bits = parseInt(value);
-      if (bits>=0 && bits<=1023 && (bits&3)==0)
-        return (bits>>2)<<offset;
-      throw "Invalid number '"+value+"' - must be between 0 and 1020 and a multiple of 4";
-    };
-  }
-  
-  function sint8_shr1_or_label(offset) {
-    return function(value) {
-      var bits = -100000000;
-      // TODO: label
-      if (value[0]=="#") {      
-        var bits = parseInt(value.substr(1));        
-      }
-      if (bits>=-256 && bits<=255 && (bits&1)==0)
-        return ((bits>>1) & 0xFF)<<offset;
-      throw "Invalid number '"+value+"' - must be between -256 and 255 and a multiple of 2";
-    };
-  }  
-   
-  function uint8(offset) {
-    return function(value) {
-      var bits = parseInt(value);
-      if (bits>=0 && bits<=255)
-        return bits<<offset;
-      throw "Invalid number '"+value+"' - must be between 0 and 255"
-    };
-  }
-  
-  function uint5(offset) {
-    return function(value) {
-      var bits = parseInt(value);
-      if (bits>=0 && bits<=63)
-        return bits<<offset;
-      throw "Invalid number '"+value+"' - must be between 0 and 63"
-    };
-  }  
-   
-  function uint5_shr2(offset) {
-    return function(value) {
-      var bits = parseInt(value);
-      if (bits>=0 && bits<=63*4)
-        return (bits>>2)<<offset;
-      throw "Invalid number '"+value+"' - must be between 0 and 255 and a multiple of 4"
-    };
-  }
-  
+
   function thumb2_immediate_t3(value) {
-    var v = parseInt(value);
+    if (value[0]!="#") 
+      throw new "Expecting '#' before number";
+    var v = parseInt(value.substr(1));
     if (v>=0 && v<65536) {
       // https://web.eecs.umich.edu/~prabal/teaching/eecs373-f11/readings/ARMv7-M_ARM.pdf page 347
       var imm4,i,imm3,imm8; // what the...?
@@ -131,15 +75,54 @@
     throw "Invalid number '"+value+"' - must be between 0 and 65535";
   }
   
+  function _int(offset, bits, shift, signed) {
+    return function(value, labels) {
+      var maxValue = ((1<<bits)-1) << shift;
+      var minValue = 0;
+      if (signed) {
+        minValue = -(1<<(bits-1));
+        maxValue += minValue;
+      }
+      
+      var binValue = undefined;
+      if (value[0]=="#") {      
+        binValue = parseInt(value.substr(1));        
+      } else {
+        if (value in labels)
+          binValue = labels[value] - labels["PC"];
+        else
+          throw "Unknown label '"+value+"'";
+      } 
+      
+      
+      console.log("VALUE----------- "+binValue+" PC "+labels["PC"]+" L "+labels[value]);
+      
+      if (binValue>=minValue && binValue<=maxValue && (binValue&((1<<shift)-1))==0)
+        return ((binValue >> shift) & ((1<<bits)-1)) << offset;
+      
+      var msg = "Invalid number '"+value+"' ("+binValue+") - must be between 0 and "+maxValue;
+      if (shift!=0) msg += " and a multiple of "+(1<<shift);
+      throw msg;
+    };
+  }
+  
+  function uint(offset, bits, shift) {
+    return _int(offset, bits, shift, false);
+  }
+  
+  function sint(offset, bits, shift) {
+    return _int(offset, bits, shift, true);
+  }  
+  
   var ops = { 
     // Format 1: move shifted register
-    "lsl"  :[{ base:"00000-----___---", regex : /(r[0-7]),(r[0-7]),#([0-9]+)/, args:[reg(0),reg(3),uint5(6)] }],
-    "lsr"  :[{ base:"00001-----___---", regex : /(r[0-7]),(r[0-7]),#([0-9]+)/, args:[reg(0),reg(3),uint5(6)] }],
-    "asr"  :[{ base:"00010-----___---", regex : /(r[0-7]),(r[0-7]),#([0-9]+)/, args:[reg(0),reg(3),uint5(6)] }],
+    "lsl"  :[{ base:"00000-----___---", regex : /(r[0-7]),(r[0-7]),(#[0-9]+)/, args:[reg(0),reg(3),uint(6,5,0)] }],
+    "lsr"  :[{ base:"00001-----___---", regex : /(r[0-7]),(r[0-7]),(#[0-9]+)/, args:[reg(0),reg(3),uint(6,5,0)] }],
+    "asr"  :[{ base:"00010-----___---", regex : /(r[0-7]),(r[0-7]),(#[0-9]+)/, args:[reg(0),reg(3),uint(6,5,0)] }],
     // 5.2 Format 2: add/subtract
     // 00011
     // 5.3 Format 3: move/compare/add/subtract immediate
-    "cmp"  :[{ base:"00101---________", regex : /(r[0-7]),#([0-9]+)/, args:[reg(8),uint8()] }], // move/compare/subtract immediate
+    "cmp"  :[{ base:"00101---________", regex : /(r[0-7]),(#[0-9]+)/, args:[reg(8),uint(0,8,0)] }], // move/compare/subtract immediate
     // 5.4 Format 4: ALU operations
     // 5.5 Format 5: Hi register operations/branch exchange
     // 5.6 Format 6: PC-relative load             
@@ -157,20 +140,20 @@
     // 5.14 Format 14: push/pop registers
     //  done (below)
     // 5.16 Format 16: conditional branch
-    "beq" :[{ base:"11010000________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
-    "bne" :[{ base:"11010001________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
-    "bcs" :[{ base:"11010010________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
-    "bcc" :[{ base:"11010011________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
-    "bmi" :[{ base:"11010100________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
-    "bpl" :[{ base:"11010101________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
-    "bvs" :[{ base:"11010110________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
-    "bvc" :[{ base:"11010111________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
-    "bhi" :[{ base:"11011000________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
-    "bls" :[{ base:"11011001________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
-    "bge" :[{ base:"11011010________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
-    "blt" :[{ base:"11011011________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
-    "bgt" :[{ base:"11011100________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
-    "ble" :[{ base:"11011101________", regex : /^(.*)$/, args:[sint8_shr1_or_label(0)] }], // 5.16 Format 16: conditional branch
+    "beq" :[{ base:"11010000________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
+    "bne" :[{ base:"11010001________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
+    "bcs" :[{ base:"11010010________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
+    "bcc" :[{ base:"11010011________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
+    "bmi" :[{ base:"11010100________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
+    "bpl" :[{ base:"11010101________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
+    "bvs" :[{ base:"11010110________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
+    "bvc" :[{ base:"11010111________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
+    "bhi" :[{ base:"11011000________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
+    "bls" :[{ base:"11011001________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
+    "bge" :[{ base:"11011010________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
+    "blt" :[{ base:"11011011________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
+    "bgt" :[{ base:"11011100________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
+    "ble" :[{ base:"11011101________", regex : /^(.*)$/, args:[sint(0,8,1)] }], // 5.16 Format 16: conditional branch
     // 5.17 Format 17: software interrupt
     // 5.18 Format 18: unconditional branch
     // 5.19 Format 19: long branch with link
@@ -179,34 +162,35 @@
     
     "push" :[{ base:"1011010-________", regex : /^{(.*)}$/, args:[rlist_lr] }], // 5.14 Format 14: push/pop registers
     "pop"  :[{ base:"1011110-________", regex : /^{(.*)}$/, args:[rlist_lr] }], // 5.14 Format 14: push/pop registers
-    "add"  :[{ base:"00110---________", regex : /(r[0-7]),#([0-9]+)/, args:[reg(8),uint8()] }, // move/compare/subtract immediate
-             { base:"10100---________", regex : /^(r[0-7]),pc,#([0-9]+)$/,args:[reg(8),uint8_shr2(0)] },
-             { base:"10101---________", regex : /^(r[0-7]),sp,#([0-9]+)$/, args:[reg(8),uint8_shr2(0)] },
-             { base:"101100000_______", regex : /^sp,#([0-9]+)$/, args:[uint7_shr2(0)] }
+    "add"  :[{ base:"00110---________", regex : /(r[0-7]),(#[0-9]+)/, args:[reg(8),uint(0,8,0)] }, // move/compare/subtract immediate
+             { base:"10100---________", regex : /^(r[0-7]),pc,(#[0-9]+)$/,args:[reg(8),uint(0,8,2)] },
+             { base:"10101---________", regex : /^(r[0-7]),sp,(#[0-9]+)$/, args:[reg(8),uint(0,8,2)] },
+             { base:"101100000_______", regex : /^sp,(#[0-9]+)$/, args:[uint(0,7,2)] }
   /*         { base:"00011-0___---___", regex : /^(r[0-7]),(r[0-7]),([^,]+)$/, args:[reg(0),reg(3),reg_or_immediate(6,10)] } */], // ?
     "adds" :[{ base:"00011-0___---___", regex : /^(r[0-7]),(r[0-7]),([^,]+)$/, args:[reg(0),reg(3),reg_or_immediate(6,10)] } ],
     "adc.w":[{ base:"111010110100----________--------", regex : /^(r[0-7]),(r[0-7]),(r[0-7])$/,args:[reg(16),reg(8),reg(0)] }], // made this up. probably wrong
-    "add.w":[{ base:"11110001--------________--------", regex : /^(r[0-7]),(r[0-7]),#([0-9]+)$/,args:[reg(16),reg(8),uint8(0)] }], // made this up. probably wrong
-    "sub"  :[{ base:"00111---________", regex : /(r[0-7]),#([0-9]+)/, args:[reg(8),uint8()] }, // move/compare/subtract immediate
-              /*{ base:"10100---________", regex : /^([^,]+),pc,#([0-9]+)$/,args:[reg(8),uint8_shr2(0)] },*/
-             { base:"101100001_______", regex : /^sp,#([0-9]+)$/, args:[uint7_shr2(0)] },
+    "add.w":[{ base:"11110001--------________--------", regex : /^(r[0-7]),(r[0-7]),(#[0-9]+)$/,args:[reg(16),reg(8),uint(0,8,0)] }], // made this up. probably wrong
+    "sub"  :[{ base:"00111---________", regex : /(r[0-7]),(#[0-9]+)/, args:[reg(8),uint(0,8,0)] }, // move/compare/subtract immediate
+              /*{ base:"10100---________", regex : /^([^,]+),pc,(#[0-9]+)$/,args:[reg(8),uint(0,8,2)] },*/
+             { base:"101100001_______", regex : /^sp,(#[0-9]+)$/, args:[uint(0,7,2)] },
              { base:"00011-1___---___", regex : /^([^,]+),([^,]+),([^,]+)$/, args:[reg(0),reg(3),reg_or_immediate(6,10)] } ],
    
     "str"  :[{ base:"0101000---___---", regex : /(r[0-7]),\[(r[0-7]),(r[0-7])\]/, args:[reg(0),reg(3),reg(6)] }, // 5.7 Format 7: load/store with register offset 
-             { base:"0110000---___---", regex : /(r[0-7]),\[(r[0-7]),#([0-9]+)\]/, args:[reg(0),reg(3), uint5_shr2(6)] }], // 5.9 Format 9: load/store with immediate offset 
+             { base:"0110000---___---", regex : /(r[0-7]),\[(r[0-7]),(#[0-9]+)\]/, args:[reg(0),reg(3), uint(6,5,2)] }], // 5.9 Format 9: load/store with immediate offset 
     "strb" :[{ base:"0101010---___---", regex : /(r[0-7]),\[(r[0-7]),(r[0-7])\]/, args:[reg(0),reg(3),reg(6)] }, // 5.7 Format 7: load/store with register offset
-             { base:"0111000---___---", regex : /(r[0-7]),\[(r[0-7]),#([0-9]+)\]/, args:[reg(0),reg(3), uint5_shr2(6)] }], // 5.9 Format 9: load/store with immediate offset 
-    "ldr"  :[{ base:"01001---________", regex : /(r[0-7]),\[pc,#([0-9]+)\]/, args:[reg(8),uint8_shr2(0)] }, // 5.6 Format 6: PC-relative load             
+             { base:"0111000---___---", regex : /(r[0-7]),\[(r[0-7]),(#[0-9]+)\]/, args:[reg(0),reg(3), uint(6,5,2)] }], // 5.9 Format 9: load/store with immediate offset 
+    "ldr"  :[{ base:"01001---________", regex : /(r[0-7]),\[pc,(#[0-9]+)\]/, args:[reg(8),uint(0,8,2)] }, // 5.6 Format 6: PC-relative load             
+             { base:"01001---________", regex : /(r[0-7]),([a-zA-Z_]+)/, args:[reg(8),uint(0,8,2)] }, // 5.6 Format 6: PC-relative load (using label)
              { base:"0101100---___---", regex : /(r[0-7]),\[(r[0-7]),(r[0-7])\]/, args:[reg(0),reg(3),reg(6)] }, // 5.7 Format 7: load/store with register offset      
-             { base:"0110100---___---", regex : /(r[0-7]),\[(r[0-7]),#([0-9]+)\]/, args:[reg(0),reg(3), uint5_shr2(6)] }], // 5.9 Format 9: load/store with immediate offset
+             { base:"0110100---___---", regex : /(r[0-7]),\[(r[0-7]),(#[0-9]+)\]/, args:[reg(0),reg(3), uint(6,5,2)] }], // 5.9 Format 9: load/store with immediate offset
     
     "ldrb" :[{ base:"0101110---___---", regex : /(r[0-7]),\[(r[0-7]),(r[0-7])\]/, args:[reg(0),reg(3),reg(6)] }, // 5.7 Format 7: load/store with register offset 
-             { base:"0110100---___---", regex : /(r[0-7]),\[(r[0-7]),#([0-9]+)\]/, args:[reg(0),reg(3), uint5_shr2(6)] }], // 5.9 Format 9: load/store with immediate offset 
-    "mov"  :[{ base:"00100---________", regex : /(r[0-7]),#([0-9]+)/, args:[reg(8),uint8()] }, // move/compare/subtract immediate
+             { base:"0110100---___---", regex : /(r[0-7]),\[(r[0-7]),(#[0-9]+)\]/, args:[reg(0),reg(3), uint(6,5,2)] }], // 5.9 Format 9: load/store with immediate offset 
+    "mov"  :[{ base:"00100---________", regex : /(r[0-7]),(#[0-9]+)/, args:[reg(8),uint(0,8,0)] }, // move/compare/subtract immediate
              { base:"0100011000---___", regex : /(r[0-7]),(r[0-7])/, args:[reg(0),reg(3)] },
              { base:"0100011010---101", regex : /sp,(r[0-7])/, args:[reg(3)] }], // made up again
-    "movs" :[{ base:"00100---________", regex : /(r[0-7]),#([0-9]+)/, args:[reg(8),uint8(0)] }],
-    "movw" :[{ base:"11110-100100----0___----________", regex : /(r[0-7]),#([0-9]+)/, args:[reg4(8),thumb2_immediate_t3] }],
+    "movs" :[{ base:"00100---________", regex : /(r[0-7]),(#[0-9]+)/, args:[reg(8),uint(0,8,0)] }],
+    "movw" :[{ base:"11110-100100----0___----________", regex : /(r[0-7]),(#[0-9]+)/, args:[reg4(8),thumb2_immediate_t3] }],
     "bx"   :[{ base:"0100011101110000", regex : /lr/, args:[] }], // made up again
     ".word":[{ base:"--------------------------------", regex : /0x([0-9A-Fa-f]+)/, args:[function(v){v=parseInt(v,16);return (v>>16)|(v<<16);}] }], // made up again
     "nop"  :[{ base:"1011111100000000", regex : "", args:[] }], // made up again
@@ -227,11 +211,23 @@
    return opCode;
   }
    
-  function assemble(asmLines, wordCallback) {
+  function assemble_internal(asmLines, wordCallback, labels) {
+    var addr = 0;
+    var newLabels = {};
     asmLines.forEach(function (line) {
+      // setup labels
+      if (labels!==undefined)
+        labels["PC"] = addr+4;
+      // handle line
       line = line.trim();
-      //console.log("'"+line+"'");
       if (line=="") return;
+      if (line.substr(-1)==":") {
+        // it's a label
+        newLabels[line.substr(0,line.length-1)] = addr;
+        return;
+      }
+      
+      // parse instruction
       var firstArgEnd = line.indexOf("\t");
       if (firstArgEnd<0) firstArgEnd = line.indexOf(" ");
       if (firstArgEnd<0) firstArgEnd=line.length;
@@ -247,27 +243,41 @@
           found = true;
           // work out the base opcode
           var opCode = getOpCode(op.base);
-          // remove the first element
-          for (var i in op.args) {
-            //console.log(i,m[(i|0)+1]);
-            var argFunction = op.args[i];
-            var bits = argFunction(m[(i|0)+1]);
-            //console.log("  ",bits)
-            opCode |= bits;
+          
+          if (labels!==undefined) {
+            /* If we're properly generating code, parse each argument.
+             Otherwise we're just working out the size in bytes of each line
+             and we can skip this */
+            for (var i in op.args) {
+              //console.log(i,m[(i|0)+1]);
+              var argFunction = op.args[i];
+              var bits = argFunction(m[(i|0)+1], labels);
+              //console.log("  ",bits)
+              opCode |= bits;
+            }
           }
+          
           if (op.base.length > 16) {
             wordCallback((opCode>>>16)); 
             wordCallback(opCode&0xFFFF);
-          } else 
+            addr += 4;
+          } else {
             wordCallback(opCode);
+            addr += 2; 
+          }
           break;
         }
       }
       // now parse args
       if (!found)
-        throw "Unknown arg style '"+args+"' in '"+line+"'";
+        throw "Unknown arg style '"+args+"' in '"+line+"'";      
     });
-    
+    return newLabels;
+  }
+
+  function assemble(asmLines, wordCallback) {    
+    var labels = assemble_internal(asmLines, function() {}, undefined);
+    assemble_internal(asmLines, wordCallback, labels);
   }
   
 //---------------------------------------------------------------------------
@@ -366,3 +376,4 @@
     init : init
   };
 }());
+
