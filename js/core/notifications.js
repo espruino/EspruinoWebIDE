8a0722ea3b0ad2dcdeb9b2b92555754955ca4903
Gordon Williams <gw@pur3.co.uk>
2015-02-13 08:20:14 +0000

    increase time notifications stay for

--- js/core/notifications.js
+++ js/core/notifications.js
@@ -23,7 +23,7 @@
       "showDuration": "300",
       "hideDuration": "1000",
       "timeOut": "5000",
-      "extendedTimeOut": "1000",
+      "extendedTimeOut": "5000",
       "showEasing": "swing",
       "hideEasing": "linear",
       "showMethod": "fadeIn",
