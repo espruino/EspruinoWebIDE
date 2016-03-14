3908a9fbf3e9ca563ebe3c48701596a22317e48f
Gordon Williams <gw@pur3.co.uk>
2016-03-14 13:00:59 +0000

    another electron tweak

--- electron-loader.js
+++ electron-loader.js
@@ -8,13 +8,24 @@ var electronPath = require( 'electron-prebuilt' ) ;
 var childProcess = require( 'child_process' ) ;
 
 // Adjust the command line arguments: remove the "node <code.js>" part
-console.log(process.argv);
 var args = process.argv.slice( 2 ) ;  
-// ... and insert the root path of our application (it's the parent directory)
-// as the first argument
-args.unshift( __dirname );
-args.unshift( "./electron-app.js" );
-console.log(args);
+// ... and insert the root path of our application as the first argument
+args.unshift( __dirname  + "/electron-app.js" );
+
+p.on('close', function(code) {
+  console.log('Loading electron...');
+});
 
 // Run electron
-childProcess.spawn( electronPath , args , { stdio: 'inherit' } ) ;  
+var p = childProcess.spawn( electronPath , args , { stdio: 'inherit' } ) ;  
+// print output
+p.stdout.on('data', function(data) {
+  console.log(data);
+});
+p.stderr.on('data', function(data) {
+  console.log(data);
+});
+// handle close
+p.on('close', function(code) {
+  console.log('Child process ended with code ' + code);
+});
