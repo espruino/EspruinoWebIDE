6a617949d8a8263dfe2b3501a6522e8c0b5d706d
Gordon Williams <gw@pur3.co.uk>
2016-03-14 13:14:15 +0000

    oops

--- electron-loader.js
+++ electron-loader.js
@@ -12,19 +12,9 @@ var args = process.argv.slice( 2 ) ;
 // ... and insert the root path of our application as the first argument
 args.unshift( __dirname  + "/electron-app.js" );
 
-p.on('close', function(code) {
-  console.log('Loading electron...');
-});
-
 // Run electron
+console.log('Loading Electron...');
 var p = childProcess.spawn( electronPath , args , { stdio: 'inherit' } ) ;  
-// print output
-p.stdout.on('data', function(data) {
-  console.log(data);
-});
-p.stderr.on('data', function(data) {
-  console.log(data);
-});
 // handle close
 p.on('close', function(code) {
   console.log('Child process ended with code ' + code);
