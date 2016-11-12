51ec7e8e6e6f5e329d0087b4359c0f13e18bb098
Gordon Williams <gw@pur3.co.uk>
2016-11-12 12:29:41 +0000

    Fix documentation when flashing Espruino WiFi

--- js/core/menuFlasher.js
+++ js/core/menuFlasher.js
@@ -4,21 +4,21 @@
  This Source Code is subject to the terms of the Mozilla Public
  License, v2.0. If a copy of the MPL was not distributed with this
  file, You can obtain one at http://mozilla.org/MPL/2.0/.
- 
+
  ------------------------------------------------------------------
   Step by step flasher
  ------------------------------------------------------------------
 **/
 "use strict";
 (function(){
-  
+
   function init() {
   }
-  
+
   function showFlasher(urlOrNothing) {
     if (urlOrNothing) urlOrNothing = urlOrNothing.trim();
     if (urlOrNothing=="") urlOrNothing=undefined;
-    
+
     if (!urlOrNothing) {
       var env = Espruino.Core.Env.getData();
       if (env!==undefined &&
@@ -27,20 +27,20 @@
         stepSelectBinary(env.BOARD, env.info, env.chip);
         return;
       }
-    }     
-    
+    }
+
     stepSelectBoard(urlOrNothing);
   }
-  
+
   function stepSelectBoard( urlOrNothing ) {
     var boardList;
-    
+
     var popup = Espruino.Core.App.openPopup({
       title: "Firmware Update",
       padding: true,
       contents: '<p>We need to find out which board you have. Please select from the list below and click next...</p>'+
                 '<div class="board_list">Loading...</div>'+
-                '<p>If you don\'t see your board here, you can\'t update the firmware on it from the IDE. Please click outside this window to close it, and <a href="http://www.espruino.com/Download" target="_blank">see the download page</a> for more instructions.</p>' ,                
+                '<p>If you don\'t see your board here, you can\'t update the firmware on it from the IDE. Please click outside this window to close it, and <a href="http://www.espruino.com/Download" target="_blank">see the download page</a> for more instructions.</p>' ,
       position: "center",
       next : function() {
         var boardId = $('.board_list option:selected').attr("name");
@@ -56,9 +56,9 @@
         }
       }
     });
-    
+
     Espruino.Core.Env.getBoardList(function(data) {
-      var html = "<red>Error loading boards...</red>";      
+      var html = "<red>Error loading boards...</red>";
       if (data) {
         boardList = data;
         html = "<select>";
@@ -75,21 +75,30 @@
         }
         html += "</select>";
       }
-      
+
       $(".board_list").html(html);
     });
-    
+
   }
 
   function stepSelectBinary(boardId, boardInfo, boardChip) {
-    // Just one firmware image - go to next!
-    if (boardInfo["binaries"]===undefined) {
-      stepReset( { binary_url : boardInfo["binary_url"], board_id : boardId, board_chip : boardChip } );
+    var binaries = boardInfo["binaries"];
+    var base_url = boardInfo["binary_url"];
+    // Old-style, and just one firmware image - go to next!
+    if (binaries===undefined) {
+      stepReset( { binary_url : base_url, board_id : boardId, board_chip : boardChip } );
+      return;
+    }
+    // New style
+    base_url = base_url.substr(0,base_url.lastIndexOf("/")+1);
+    // Just one...
+    if (binaries.length==1) {
+      var binary_url = base_url+binaries[0]["filename"].replace("%v", boardInfo["binary_version"]);
+      stepReset( { binary_url : binary_url, board_id : boardId, board_chip : boardChip } );
       return;
     }
     // More than one...
     // Make a list
-    var binaries = boardInfo["binaries"];
     var html = "<select class=\"fw_list\">";
     for (var i in binaries) {
       try {
@@ -99,13 +108,13 @@
       }
     }
     html += "</select>";
-    // Crearte popup
+    // Create popup
     var popup = Espruino.Core.App.openPopup({
       title: "Firmware Update",
       padding: true,
       contents: '<p>Your board has multiple options for firmware. Please select from the list below and click next...</p>'+
                 '<p><b>Note:</b> If you don\'t need any of the features listed, you can choose any firmware.</p>'+
-                html,                
+                html,
       position: "center",
       next : function() {
         var binary_filename = $('.fw_list option:selected').attr("filename");
@@ -113,25 +122,23 @@
         if (binary_filename===undefined)
           console.error("No binary filename found! Looks like no option selected");
         else {
-          var base_url = boardInfo["binary_url"];
-          base_url = base_url.substr(0,base_url.lastIndexOf("/")+1);          
           var binary_url = base_url+binary_filename.replace("%v", boardInfo["binary_version"]);
           console.log("Choosing "+binary_url);
           stepReset( { binary_url : binary_url, board_id : boardId, board_chip : boardChip } );
         }
       }
     });
-    
+
   }
-  
-  
+
+
   function stepReset(data) {
     Espruino.Core.MenuPortSelector.disconnect();
-    
+
     var popup = Espruino.Core.App.openPopup({
       title: "Firmware Update",
       padding: true,
-      contents: getDocs(data, "reset"),                
+      contents: getDocs(data, "reset"),
       position: "center",
       next : function() {
         popup.close();
@@ -139,30 +146,30 @@
       }
     });
   }
-  
+
   function stepFlash(data) {
     Espruino.Core.MenuPortSelector.ensureConnected(function() {
       console.log("stepFlash: ",data);
       var url = data.binary_url;
       var flashOffset = data.board_chip.place_text_section;
-      
+
       var popup = Espruino.Core.App.openPopup({
         title: "Firmware Update",
         padding: true,
         contents: '<p><b>Your firmware is now being updated</b>...</p>'+
-                  '<p>See the status bar below to find out what\'s happening...</p>' ,                
+                  '<p>See the status bar below to find out what\'s happening...</p>' ,
         position: "center",
       });
-    
+
       Espruino.Core.Flasher.flashDevice(url, flashOffset, function (err) {
         Espruino.Core.Terminal.grabSerialPort();
         Espruino.Core.MenuPortSelector.disconnect();
         popup.close();
         if (err) {
-          Espruino.Core.Notifications.error("Error Flashing: "+ err, true);        
+          Espruino.Core.Notifications.error("Error Flashing: "+ err, true);
           console.log(err);
           stepError(err);
-        } else {        
+        } else {
           Espruino.Core.Notifications.success("Flashing Complete", true);
           Espruino.callProcessor("flashComplete");
           stepSuccess(data);
@@ -170,67 +177,68 @@
       });
     });
   }
-  
+
   function stepSuccess(data) {
     var popup = Espruino.Core.App.openPopup({
       title: "Firmware Update",
       padding: true,
       contents: '<p><b>The Firmware was updated successfully!</b><p>'+
-                getDocs(data, "success"),                
+                getDocs(data, "success"),
       position: "center",
       next : function() {
         popup.close();
       }
     });
-  }  
-  
+  }
+
   function stepError(err) {
     var popup = Espruino.Core.App.openPopup({
       title: "Firmware Update",
       padding: true,
       contents: '<p><b>Sorry, the firmware update has failed.</b></p>'+
                 '<p>The error was: <i>'+err+'</i></p>'+
-                '<p>Please try again, or check out the <a href="http://www.espruino.com/Troubleshooting" target="_blank">Troubleshooting page</a> for what to do next.</p>',                
+                '<p>Please try again, or check out the <a href="http://www.espruino.com/Troubleshooting" target="_blank">Troubleshooting page</a> for what to do next.</p>',
       position: "center",
       next : function() {
         popup.close();
       }
     });
   }
-  
+
   function getDocs(data, doc) {
     var html = undefined;
     if (doc=="reset") {
-      if (data.board_id.substr(0,4)=="PICO") {
-        html = 
+      if (data.board_id.substr(0,4)=="PICO" ||
+          data.board_id=="ESPRUINOWIFI") {
+        html =
           '<p><b>Please put your board into bootloader mode.</b> Plug it into USB with the button held down.</p>'+
           '<p>When the red and green LEDs start pulsing on and off, click \'Next\'...</p>'+
           '<p>If the LEDs are not pulsing, please see the <a href="http://www.espruino.com/Troubleshooting" target="_blank">Troubleshooting page</a></p>';
-      } else {        
-        html = 
+      } else {
+        html =
           '<p><b>Please put your board into bootloader mode.</b> Hold down BTN1, and then press and release RST.</p>'+
           '<p>When the blue LED starts pulsing on and off, click \'Next\'...</p>'+
           '<p>If the blue LED is not pulsing, please see the <a href="http://www.espruino.com/Troubleshooting" target="_blank">Troubleshooting page</a></p>';
       }
     } else if (doc=="success") {
-      if (data.board_id.substr(0,4)=="PICO") {
-        html = 
-          '<p>Please unplug the Pico and plug it back in to exit bootloader mode, then click Next to start using it!</p>';
-      } else {        
-        html = 
+      if (data.board_id.substr(0,4)=="PICO"  ||
+          data.board_id=="ESPRUINOWIFI") {
+        html =
+          '<p>Please unplug the board and plug it back in to exit bootloader mode, then click Next to start using it!</p>';
+      } else {
+        html =
           '<p>Please press the RST button to reset the Espruino out of bootloader mode, then click Next to start using it!</p>';
       }
-    } 
+    }
     if (!html) {
       html = "Couldn't find documentation"
       console.warn("Unknown doc type '"+doc+"' for board '"+data.board_id+"'");
     }
     return html;
   }
-  
+
   Espruino.Core.MenuFlasher = {
       init : init,
-      
       showFlasher : showFlasher
   };
 
