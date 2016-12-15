c21e2795ae19bf1736adf563f606a6d991165839
Gordon Williams <gw@pur3.co.uk>
2016-12-15 18:17:29 +0000

    fix #157

--- js/core/file.js
+++ js/core/file.js
@@ -4,121 +4,160 @@
  This Source Code is subject to the terms of the Mozilla Public
  License, v2.0. If a copy of the MPL was not distributed with this
  file, You can obtain one at http://mozilla.org/MPL/2.0/.
- 
+
  ------------------------------------------------------------------
   File Load/Save
  ------------------------------------------------------------------
 **/
 "use strict";
 (function(){
-  
+
   var currentJSFileName = "code.js";
   var currentXMLFileName = "code_blocks.xml";
-  
+  var loadFileCallback;
+
   function init() {
     // Configuration
-    
-   
+
+
     // Add stuff we need
-    Espruino.Core.App.addIcon({ 
+    Espruino.Core.App.addIcon({
       id: "openFile",
-      icon: "folder-open", 
-      title : "Open File", 
-      order: 100, 
-      area: { 
-        name: "code", 
+      icon: "folder-open",
+      title : "Open File",
+      order: 100,
+      area: {
+        name: "code",
         position: "top"
-      }, 
+      },
       click: function() {
-        if (Espruino.Core.Code.isInBlockly()) 
+        if (Espruino.Core.Code.isInBlockly())
           loadFile(Espruino.Core.EditorBlockly.setXML, currentXMLFileName);
         else
           loadFile(Espruino.Core.EditorJavaScript.setCode, currentJSFileName);
       }
     });
 
-    Espruino.Core.App.addIcon({ 
+    Espruino.Core.App.addIcon({
       id: "saveFile",
-      icon: "save", 
-      title : "Save File", 
-      order: 200, 
-      area: { 
-        name: "code", 
+      icon: "save",
+      title : "Save File",
+      order: 200,
+      area: {
+        name: "code",
         position: "top"
       },
       click: function() {
-        if (Espruino.Core.Code.isInBlockly()) 
+        if (Espruino.Core.Code.isInBlockly())
           saveFile(Espruino.Core.EditorBlockly.getXML(), currentXMLFileName);
         else
           saveFile(Espruino.Core.EditorJavaScript.getCode(), currentJSFileName);
       }
     });
+
+    var fileLoader = document.createElement("input");
+    fileLoader.setAttribute("id", "fileLoader");
+    fileLoader.setAttribute("type", "file");
+    fileLoader.addEventListener('change', function(e) {
+      if (!loadFileCallback) return;
+      var files = e.target.files;
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        loadFileCallback(convertFromOS(e.target.result));
+        loadFileCallback = undefined;
+      };
+      // Read in the image file as a data URL.
+      reader.readAsText(files[0]);
+    }, false)
+    document.body.appendChild(fileLoader);
   }
 
   function setCurrentFileName(filename) {
     if (Espruino.Core.Code.isInBlockly()) {
       currentXMLFileName = filename;
-    } else { 
+    } else {
       currentJSFileName = filename;
     }
   }
-  
+
   /**  Handle newline conversions - Windows expects newlines as /r/n when we're saving/loading files */
   function convertFromOS(chars) {
    if (!Espruino.Core.Utils.isWindows()) return chars;
    return chars.replace(/\r\n/g,"\n");
   };
-  
+
   /**  Handle newline conversions - Windows expects newlines as /r/n when we're saving/loading files */
   function convertToOS(chars) {
    if (!Espruino.Core.Utils.isWindows()) return chars;
    return chars.replace(/\r\n/g,"\n").replace(/\n/g,"\r\n");
-  };  
+  };
 
   function loadFile(callback, filename) {
-    chrome.fileSystem.chooseEntry({type: 'openFile', suggestedName:filename}, function(fileEntry) {
-      if (!fileEntry) return;
-      if (fileEntry.name) setCurrentFileName(fileEntry.name);
-      fileEntry.file(function(file) {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-          callback(convertFromOS(e.target.result));
-        };
-        reader.onerror = function() {
-          Espruino.Core.Notifications.error("Error Loading", true);     
-        };
-        reader.readAsText(file);
+    if (chrome.fileSystem) {
+      // Chrome Web App / NW.js
+      chrome.fileSystem.chooseEntry({type: 'openFile', suggestedName:filename}, function(fileEntry) {
+        if (!fileEntry) return;
+        if (fileEntry.name) setCurrentFileName(fileEntry.name);
+        fileEntry.file(function(file) {
+          var reader = new FileReader();
+          reader.onload = function(e) {
+            callback(convertFromOS(e.target.result));
+          };
+          reader.onerror = function() {
+            Espruino.Core.Notifications.error("Error Loading", true);
+          };
+          reader.readAsText(file);
+        });
       });
-    });
+    } else {
+      loadFileCallback = callback;
+      var fileLoader = document.getElementById("fileLoader");
+      fileLoader.click();
+    }
   }
-  
+
   function saveFile(data, filename) {
     //saveAs(new Blob([convertToOS(data)], { type: "text/plain" }), filename); // using FileSaver.min.js
 
     function errorHandler() {
-      Espruino.Core.Notifications.error("Error Saving", true);     
+      Espruino.Core.Notifications.error("Error Saving", true);
     }
 
-    chrome.fileSystem.chooseEntry({type: 'saveFile', suggestedName:filename}, function(writableFileEntry) {
-      if (writableFileEntry.name)
-        setCurrentFileName(writableFileEntry.name);
-      writableFileEntry.createWriter(function(writer) {
-        var blob = new Blob([convertToOS(data)],{ type: "text/plain"} );
-        writer.onerror = errorHandler;
-        // when truncation has finished, write
-        writer.onwriteend = function(e) {
+    if (chrome.fileSystem) {
+      // Chrome Web App / NW.js
+      chrome.fileSystem.chooseEntry({type: 'saveFile', suggestedName:filename}, function(writableFileEntry) {
+        if (writableFileEntry.name)
+          setCurrentFileName(writableFileEntry.name);
+        writableFileEntry.createWriter(function(writer) {
+          var blob = new Blob([convertToOS(data)],{ type: "text/plain"} );
+          writer.onerror = errorHandler;
+          // when truncation has finished, write
           writer.onwriteend = function(e) {
-            console.log('FileWriter: complete');
+            writer.onwriteend = function(e) {
+              console.log('FileWriter: complete');
+            };
+            console.log('FileWriter: writing');
+            writer.write(blob);
           };
-          console.log('FileWriter: writing');
-          writer.write(blob);
-        };
-        // truncate
-        console.log('FileWriter: truncating');
-        writer.truncate(blob.size);
-      }, errorHandler);
-    });
-  };  
+          // truncate
+          console.log('FileWriter: truncating');
+          writer.truncate(blob.size);
+        }, errorHandler);
+      });
+    } else {
+      var a = document.createElement("a"),
+          file = new Blob([data], {type: "text/plain"});
+      var url = URL.createObjectURL(file);
+      a.href = url;
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(function() {
+          document.body.removeChild(a);
+          window.URL.revokeObjectURL(url);
+      }, 0);
+    }
+  };
 
   Espruino.Core.File = {
     init : init
--- js/plugins/fileReload.js
+++ js/plugins/fileReload.js
@@ -4,55 +4,57 @@
  This Source Code is subject to the terms of the Mozilla Public
  License, v2.0. If a copy of the MPL was not distributed with this
  file, You can obtain one at http://mozilla.org/MPL/2.0/.
- 
+
  ------------------------------------------------------------------
   A button to reload the last loaded file
  ------------------------------------------------------------------
 **/
 "use strict";
 (function(){
+  /* THIS IS BROKEN - PRs welcome :) */
+  /*
 
   var icon;
-  
+
   function init() {
     Espruino.Core.Config.add("SHOW_RELOAD_ICON", {
       section : "General",
       name : "File Reload Icon",
       description : "Show an icon that allows you to reload the contents of a file that you just opened.",
       type : "boolean",
-      defaultValue : false, 
+      defaultValue : false,
       onChange : function(newValue) { showIcon(newValue); }
-    });    
+    });
 
     showIcon(Espruino.Config.SHOW_RELOAD_ICON);
   }
 
   function showIcon(show) {
     if (show) {
-      icon = Espruino.Core.App.addIcon({ 
+      icon = Espruino.Core.App.addIcon({
         id: "reloadfile",
-        icon: "refresh", 
-        title : "Reload File", 
-        order: 300, 
-        area: { 
-          name: "code", 
+        icon: "refresh",
+        title : "Reload File",
+        order: 300,
+        area: {
+          name: "code",
           position: "top"
         },
         click: reloadFile
-      });    
+      });
     } else {
       if (icon!==undefined) icon.remove();
     }
   }
-  
+
   function reloadFile() {
     if (document.getElementById('fileLoader').value=="")
       $('#fileLoader').click();
     else
       $('#fileLoader').change();
   };
-  
+
   Espruino.Plugins.FileReload = {
     init : init,
-  };
+  };*/
 }());
