91a6f30a85c105adcc8d541cd931c100727bb2ec
Gordon Williams <gw@pur3.co.uk>
2017-01-18 09:39:24 +0000

    fix multiplatform bug

--- js/core/app.js
+++ js/core/app.js
@@ -39,7 +39,7 @@
    * Initialize the window
    */
   function init() {
-    if ((nwwindow || (chrome && chrome.app && chrome.app.window)) &&
+    if ((nwwindow || ((typeof chrome!="undefined") && chrome.app && chrome.app.window)) &&
         document.getElementsByClassName("title-bar").length) {
       Espruino.Core.App.addIcon({
         id:'minimize',
