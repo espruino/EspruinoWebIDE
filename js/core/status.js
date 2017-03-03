5320ac74bc9091d8da1271b0526c39462d4d9ac7
Gordon Williams <gw@pur3.co.uk>
2017-03-03 09:35:06 +0000

    Show connected COM port (fix #73)

--- js/core/status.js
+++ js/core/status.js
@@ -4,24 +4,34 @@
  This Source Code is subject to the terms of the Mozilla Public
  License, v2.0. If a copy of the MPL was not distributed with this
  file, You can obtain one at http://mozilla.org/MPL/2.0/.
- 
+
  ------------------------------------------------------------------
   Status menu
  ------------------------------------------------------------------
 **/
 "use strict";
 (function(){
-  
+
   var progressAmt, progressMax = 0;
-  
-  function init() 
+
+  function init()
   {
     $("<h5 class='status__message'>Ready...</h5>").appendTo(".status__left");
+    $("<h5 class='status__port'></h5>").appendTo(".status__right");
     $("<div class='status__progress'><div class='status__progress-bar' /></div>").appendTo(".status__right").hide();
+    Espruino.addProcessor("connected", function(data, callback) {
+      setConnectedPort(data.port);
+      callback(data);
+    });
+
+    Espruino.addProcessor("disconnected", function(data, callback) {
+      setConnectedPort("");
+      callback(data);
+    });
   }
-  
+
   function setStatus(text, progress) {
-    console.log(">>> "+ text);    
+    console.log(">>> "+ text);
     //var progressBox = $(".progress");
     //var statusBox = $(".progress .status");
     //var progressIndicator = $(".progress .indicator");
@@ -29,10 +39,10 @@
 
     if (progress === undefined) {
       $(".status__progress-bar").width(0);
-      $(".status__progress").hide();      
+      $(".status__progress").hide();
       progressMax = 0;
     } else {
-      $(".status__progress").show(); 
+      $(".status__progress").show();
       if (progress < 1) progress = 1;
       progressAmt = 0;
       progressMax = progress;
@@ -44,19 +54,27 @@
 
   function hasProgress() {
     return progressMax > 0;
-  };    
-  
+  };
+
   function incrementProgress(amount) {
-    if (!hasProgress()) return;      
+    if (!hasProgress()) return;
     progressAmt += amount;
     var progressPercent = (100 / progressMax) * progressAmt;
     $(".status__progress-bar").css("width", progressPercent + "%");
-  };  
-  
+  };
+
+  function setConnectedPort(port) {
+    if (!port) port="";
+    var idx = port.lastIndexOf("/");
+    if (idx>=0 && idx<port.length-1)
+      port = port.substr(idx+1);
+    $(".status__port").html(port);
+  };
+
   Espruino.Core.Status = {
       init : init,
       setStatus : setStatus,
       hasProgress : hasProgress,
-      incrementProgress : incrementProgress,
+      incrementProgress : incrementProgress
   };
 }());
