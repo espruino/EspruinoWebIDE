5906efa171b3b00a706185c987b0fe8c8ba89f01
Gordon Williams <gw@pur3.co.uk>
2019-02-27 15:34:08 +0000

    Handle smartibot blocks

--- blockly/blockly_smartibot.js
+++ blockly/blockly_smartibot.js
@@ -14,205 +14,103 @@
 // ---------------------------------
 var SMARTIBOT_COL = 290;
 
+const SMARTIBOT_MOTORS = [
+  ["M1", '1'],
+  ["M2", '2'],
+  ["M3", '3'],
+  ["M4", '4']];
+const SMARTIBOT_SERVOS = [
+  ["S1", '1'],
+  ["S2", '2'],
+  ["S3", '3'],
+  ["S4", '4'],
+  ["S5", '5'],
+  ["S6", '6'],
+  ["S7", '7'],
+  ["S8", '8'],
+  ["S9", '9'],
+  ["S10", '10']];
+const SMARTIBOT_COLORS = [
+  ["Orange", '[13,92,47]'],
+  ["Pink", '[24,90,53]'],
+  ["Blue", '[100,27,86]'],
+  ["Off", '[0,0,0]']];
+
 // ------------------
 
-Blockly.Blocks.smartibot_M1Fwd = {
-  category: 'Espurino',
+Blockly.Blocks.smartibot_motor = {
+  category: 'Smartibot',
   init: function() {
-      this.appendValueInput('VAL')
-          .setCheck(['Number','Boolean'])
-          .appendField(Blockly.Msg.SMARTIBOT_M1FWD);
-
+    this.appendDummyInput().appendField("Motor")
+         .appendField(new Blockly.FieldDropdown(SMARTIBOT_MOTORS), 'MOTOR')
+         .appendField("speed");
+    this.appendValueInput('VAL').setCheck(['Number','Boolean']);
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(SMARTIBOT_COL);
     this.setInputsInline(true);
-    //this.setTooltip(Blockly.Msg.ESPRUINO_M1FWD_TOOLTIP);
+    //this.setTooltip("...");
   }
 };
 
-
-Blockly.Blocks.smartibot_M1Bwd = {
-  category: 'Espurino',
+Blockly.Blocks.smartibot_servo = {
+  category: 'Smartibot',
   init: function() {
-      this.appendValueInput('VAL')
-          .setCheck(['Number','Boolean'])
-          .appendField(Blockly.Msg.SMARTIBOT_M1BWD);
-
+    this.appendDummyInput().appendField("Servo")
+         .appendField(new Blockly.FieldDropdown(SMARTIBOT_SERVOS), 'SERVO')
+         .appendField("position");
+    this.appendValueInput('VAL').setCheck(['Number','Boolean']);
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(SMARTIBOT_COL);
     this.setInputsInline(true);
-    //this.setTooltip(Blockly.Msg.ESPRUINO_M1BWD_TOOLTIP);
+    //this.setTooltip("...");
   }
 };
-
-Blockly.Blocks.smartibot_M2Fwd = {
-  category: 'Espurino',
-  init: function() {
-      this.appendValueInput('VAL')
-          .setCheck(['Number','Boolean'])
-          .appendField(Blockly.Msg.SMARTIBOT_M2FWD);
-
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(SMARTIBOT_COL);
-    this.setInputsInline(true);
-    //this.setTooltip(Blockly.Msg.ESPRUINO_M2FWD_TOOLTIP);
-  }
-};
-
-
-Blockly.Blocks.smartibot_M2Bwd = {
-  category: 'Espurino',
-  init: function() {
-      this.appendValueInput('VAL')
-          .setCheck(['Number','Boolean'])
-          .appendField(Blockly.Msg.SMARTIBOT_M2BWD);
-
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(SMARTIBOT_COL);
-    this.setInputsInline(true);
-    //this.setTooltip(Blockly.Msg.ESPRUINO_M2BWD_TOOLTIP);
-  }
-};
-
-Blockly.Blocks.smartibot_M3Fwd = {
-  category: 'Espurino',
-  init: function() {
-      this.appendValueInput('VAL')
-          .setCheck(['Number','Boolean'])
-          .appendField(Blockly.Msg.SMARTIBOT_M3FWD);
-
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(SMARTIBOT_COL);
-    this.setInputsInline(true);
-    //this.setTooltip(Blockly.Msg.ESPRUINO_M3FWD_TOOLTIP);
-  }
-};
-
-Blockly.Blocks.smartibot_M3Bwd = {
-  category: 'Espurino',
-  init: function() {
-      this.appendValueInput('VAL')
-          .setCheck(['Number','Boolean'])
-          .appendField(Blockly.Msg.SMARTIBOT_M3BWD);
-
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(SMARTIBOT_COL);
-    this.setInputsInline(true);
-    //this.setTooltip(Blockly.Msg.ESPRUINO_M3BWD_TOOLTIP);
-  }
-};
-
-Blockly.Blocks.smartibot_M4Fwd = {
-  category: 'Espurino',
-  init: function() {
-      this.appendValueInput('VAL')
-          .setCheck(['Number','Boolean'])
-          .appendField(Blockly.Msg.SMARTIBOT_M4FWD);
-
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(SMARTIBOT_COL);
-    this.setInputsInline(true);
-    //this.setTooltip(Blockly.Msg.ESPRUINO_M4FWD_TOOLTIP);
-  }
-};
-
-
-Blockly.Blocks.smartibot_M4Bwd = {
-  category: 'Espurino',
-  init: function() {
-      this.appendValueInput('VAL')
-          .setCheck(['Number','Boolean'])
-          .appendField(Blockly.Msg.SMARTIBOT_M4BWD);
-
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(SMARTIBOT_COL);
-    this.setInputsInline(true);
-    //this.setTooltip(Blockly.Msg.ESPRUINO_M4BWD_TOOLTIP);
-  }
-};
-
-// --------------------------------
 
 Blockly.Blocks.smartibot_LED = {
-  category: 'Espurino',
+  category: 'Smartibot',
   init: function() {
-      this.appendValueInput('VAL')
+      this.appendValueInput('LR')
           .setCheck(['Number'])
-          .appendField(Blockly.Msg.SMARTIBOT_LED1);
-      this.appendValueInput('VAL2')
+          .appendField("EYES Left - R:");
+      this.appendValueInput('LG')
           .setCheck(['Number'])
-          .appendField(Blockly.Msg.SMARTIBOT_LED1G);
-      this.appendValueInput('VAL3')
+          .appendField("G:");
+      this.appendValueInput('LB')
           .setCheck(['Number'])
-          .appendField(Blockly.Msg.SMARTIBOT_LED1B);
-      this.appendValueInput('VAL4')
+          .appendField("B:");
+      this.appendValueInput('RR')
           .setCheck(['Number'])
-          .appendField(Blockly.Msg.SMARTIBOT_LED2);
-      this.appendValueInput('VAL5')
+          .appendField('Right - R:');
+      this.appendValueInput('RG')
           .setCheck(['Number'])
-          .appendField(Blockly.Msg.SMARTIBOT_LED2G);
-      this.appendValueInput('VAL6')
+          .appendField("G:");
+      this.appendValueInput('RB')
           .setCheck(['Number'])
-          .appendField(Blockly.Msg.SMARTIBOT_LED2B);
+          .appendField("B:");
 
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(SMARTIBOT_COL);
     this.setInputsInline(true);
-    //this.setTooltip(Blockly.Msg.ESPRUINO_M4BWD_TOOLTIP);
+    //this.setTooltip("...");
   }
 };
 
-Blockly.Blocks.smartibot_LEDorange = {
-  category: 'Espurino',
+Blockly.Blocks.smartibot_LEDcol = {
+  category: 'Smartibot',
   init: function() {
-      this.appendValueInput('VAL')
-          .setCheck(['Number'])
-          .appendField(Blockly.Msg.SMARTIBOT_LEDORANGE);
+    this.appendDummyInput().appendField("EYES Left -")
+         .appendField(new Blockly.FieldDropdown(SMARTIBOT_COLORS), 'COL')
+         .appendField("Brightness:");
+    this.appendValueInput('BRIGHT').setCheck(['Number']);
 
     this.setPreviousStatement(true);
     this.setNextStatement(true);
-    this.setColour(35);
+    this.setColour(SMARTIBOT_COL);
     this.setInputsInline(true);
-    //this.setTooltip(Blockly.Msg.ESPRUINO_M4BWD_TOOLTIP);
-  }
-};
-
-Blockly.Blocks.smartibot_LEDpink = {
-  category: 'Espurino',
-  init: function() {
-      this.appendValueInput('VAL')
-          .setCheck(['Number'])
-          .appendField(Blockly.Msg.SMARTIBOT_LEDPINK);
-
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(310);
-    this.setInputsInline(true);
-    //this.setTooltip(Blockly.Msg.ESPRUINO_M4BWD_TOOLTIP);
-  }
-};
-
-Blockly.Blocks.smartibot_LEDblue = {
-  category: 'Espurino',
-  init: function() {
-      this.appendValueInput('VAL')
-          .setCheck(['Number'])
-          .appendField(Blockly.Msg.SMARTIBOT_LEDBLUE);
-
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(190);
-    this.setInputsInline(true);
-    //this.setTooltip(Blockly.Msg.ESPRUINO_M4BWD_TOOLTIP);
+    //this.setTooltip("...");
   }
 };
 
@@ -221,74 +119,33 @@ Blockly.Blocks.smartibot_LEDblue = {
 // -----------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------
 
-Blockly.JavaScript.smartibot_M1Fwd = function() {
+Blockly.JavaScript.smartibot_motor = function() {
+  var motor = this.getFieldValue('MOTOR');
   var val = Blockly.JavaScript.valueToCode(this, 'VAL', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  return "digitalWrite(D6, 0);\nanalogWrite(D4, "+val+");\n";
+  return 'require("Smartibot").setMotor('+motor+','+val+');\n';
 };
 
-Blockly.JavaScript.smartibot_M1Bwd = function() {
+Blockly.JavaScript.smartibot_servo = function() {
+  var servo = this.getFieldValue('SERVO');
   var val = Blockly.JavaScript.valueToCode(this, 'VAL', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  return "digitalWrite(D4, 0);\nanalogWrite(D6, "+val+");\n";
+  return 'require("Smartibot").setServo('+servo+','+val+');\n';
 };
-
-Blockly.JavaScript.smartibot_M2Fwd = function() {
-  var val = Blockly.JavaScript.valueToCode(this, 'VAL', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  return "digitalWrite(D11, 0);\nanalogWrite(D10, "+val+");\n";
-};
-
-Blockly.JavaScript.smartibot_M2Bwd = function() {
-  var val = Blockly.JavaScript.valueToCode(this, 'VAL', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  return "digitalWrite(D10, 0);\nanalogWrite(D11, "+val+");\n";
-};
-
-Blockly.JavaScript.smartibot_M3Fwd = function() {
-  var val = Blockly.JavaScript.valueToCode(this, 'VAL', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  return "digitalWrite(D3, 0);\nanalogWrite(D2, "+val+");\n";
-};
-
-Blockly.JavaScript.smartibot_M3Bwd = function() {
-  var val = Blockly.JavaScript.valueToCode(this, 'VAL', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  return "digitalWrite(D2, 0);\nanalogWrite(D3, "+val+");\n";
-};
-
-Blockly.JavaScript.smartibot_M4Fwd = function() {
-  var val = Blockly.JavaScript.valueToCode(this, 'VAL', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  return "digitalWrite(D12, 0);\nanalogWrite(D9, "+val+");\n";
-};
-
-Blockly.JavaScript.smartibot_M4Bwd = function() {
-  var val = Blockly.JavaScript.valueToCode(this, 'VAL', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  return "digitalWrite(D9, 0);\nanalogWrite(D12, "+val+");\n";
-};
-
-// ---------------------
 
 Blockly.JavaScript.smartibot_LED = function() {
-  var lr = Blockly.JavaScript.valueToCode(this, 'VAL', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  var lg = Blockly.JavaScript.valueToCode(this, 'VAL2', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  var lb = Blockly.JavaScript.valueToCode(this, 'VAL3', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  var rr = Blockly.JavaScript.valueToCode(this, 'VAL4', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  var rg = Blockly.JavaScript.valueToCode(this, 'VAL5', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  var rb = Blockly.JavaScript.valueToCode(this, 'VAL6', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+  var lr = Blockly.JavaScript.valueToCode(this, 'LR', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+  var lg = Blockly.JavaScript.valueToCode(this, 'LG', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+  var lb = Blockly.JavaScript.valueToCode(this, 'LB', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+  var rr = Blockly.JavaScript.valueToCode(this, 'RR', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+  var rg = Blockly.JavaScript.valueToCode(this, 'RG', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+  var rb = Blockly.JavaScript.valueToCode(this, 'RB', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
   var l = JSON.stringify([lr,lg,lb]);
   var r = JSON.stringify([rr,rg,rb]);
   return 'require("Smartibot").setLEDs('+l+','+r+');\n';
 };
 
-Blockly.JavaScript.smartibot_LEDorange = function() {
-  var val = Blockly.JavaScript.valueToCode(this, 'VAL', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  var c = JSON.stringify([13*val,92*val,47*val]);
-  return 'require("Smartibot").setLEDs('+c+','+c+');\n';
-};
-
-Blockly.JavaScript.smartibot_LEDpink = function() {
-  var val = Blockly.JavaScript.valueToCode(this, 'VAL', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  var c = JSON.stringify([24*val,90*val,53*val]);
-  return 'require("Smartibot").setLEDs('+c+','+c+');\n';
-};
-
-Blockly.JavaScript.smartibot_LEDblue = function() {
-  var val = Blockly.JavaScript.valueToCode(this, 'VAL', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  var c = JSON.stringify([100*val,27*val,86*val]);
-  return 'require("Smartibot").setLEDs('+c+','+c+');\n';
+Blockly.JavaScript.smartibot_LEDcol = function() {
+  var col = this.getFieldValue('COL');
+  var bright = Blockly.JavaScript.valueToCode(this, 'BRIGHT', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+  col = col+'.map(x=>x*'+bright+')';
+  return 'require("Smartibot").setLEDs('+col+','+col+');\n';
 };
