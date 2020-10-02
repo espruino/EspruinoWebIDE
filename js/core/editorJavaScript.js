5d11f54880ccf8c7b2ad56ead729bdcf6c4a5d87
Huntly Cameron <huntly.cameron@gmail.com>
2020-10-02 19:25:37 +0100

    Resolves #218 - syntax highlghting added to JavaScript Editor

--- js/core/editorJavaScript.js
+++ js/core/editorJavaScript.js
@@ -24,6 +24,14 @@
       defaultValue : "sublime",
       onChange : function(newValue) { codeMirror.keyMap = Espruino.Config.KEYMAP }
     });
+    Espruino.Core.Config.add("THEME", {
+      section : "General",
+      name : "JavaScript Editor Theme",
+      description : "Changes the colour scheme for the JavaScript editor.",
+      type : { "default": "default", "3024-day": "3024-day", "3024-night": "3024-night", "abcdef": "abcdef", "ambiance": "ambiance", "ayu-dark": "ayu-dark", "ayu-mirage": "ayu-mirage", "base16-dark": "base16-dark", "base16-light": "base16-light", "bespin": "bespin", "blackboard": "blackboard", "cobalt": "cobalt", "colorforth": "colorforth", "darcula": "darcula", "dracula": "dracula", "duotone-dark": "duotone-dark", "duotone-light": "duotone-light", "eclipse": "eclipse", "elegant": "elegant", "erlang-dark": "erlang-dark", "gruvbox-dark": "gruvbox-dark", "hopscotch": "hopscotch", "icecoder": "icecoder", "idea": "idea", "isotope": "isotope", "lesser-dark": "lesser-dark", "liquibyte": "liquibyte", "lucario": "lucario", "material": "material", "material-darker": "material-darker", "material-palenight": "material-palenight", "material-ocean": "material-ocean", "mbo": "mbo", "mdn-like": "mdn-like", "midnight": "midnight", "monokai": "monokai", "moxer": "moxer", "neat": "neat", "neo": "neo", "night": "night", "nord": "nord", "oceanic-next": "oceanic-next", "panda-syntax": "panda-syntax", "paraiso-dark": "paraiso-dark", "paraiso-light": "paraiso-light", "pastel-on-dark": "pastel-on-dark", "railscasts": "railscasts", "rubyblue": "rubyblue", "seti": "seti", "shadowfox": "shadowfox", "solarized dark": "solarized dark", "solarized light": "solarized light", "the-matrix": "the-matrix", "tomorrow-night-bright": "tomorrow-night-bright", "tomorrow-night-eighties": "tomorrow-night-eighties", "ttcn": "ttcn", "twilight": "twilight", "vibrant-ink": "vibrant-ink", "xq-dark": "xq-dark", "xq-light": "xq-light", "yeti": "yeti", "yonce": "yonce", "zenburn": "zenburn" },
+      defaultValue : "default",
+      onChange : function(newValue) { codeMirror.theme = Espruino.Config.THEME }
+    });
     Espruino.Core.Config.add("DISABLE_CODE_HINTS", {
       section : "General",
       name : "Disable Code Hints",
@@ -33,7 +41,6 @@
       type : "boolean",
       defaultValue : false,
     });
-
     $('<div id="divcode" style="width:100%;height:100%;"><textarea id="code" name="code"></textarea></div>').appendTo(".editor--code .editor__canvas");
     // The code editor
     var lintFlags = {
@@ -56,6 +63,7 @@
       foldGutter: {rangeFinder: new CodeMirror.fold.combine(CodeMirror.fold.brace, CodeMirror.fold.comment, CodeMirror.fold.indent)},
       gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter", "CodeMirror-lint-markers"],
       keyMap: Espruino.Config.KEYMAP,
+      theme: Espruino.Config.THEME,
       extraKeys: {
         "Tab" : function(cm) {
           if (cm.somethingSelected()) {
