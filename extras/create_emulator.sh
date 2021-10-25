16e07e4902f55355fe97dbf197bc3129241b115a
Gordon Williams <gw@pur3.co.uk>
2021-10-25 17:06:06 +0100

    New emulator, and fix out of bounds touch events

--- extras/create_emulator.sh
+++ extras/create_emulator.sh
@@ -4,7 +4,8 @@ DIR_IDE=`pwd`
 echo $DIR_IDE
 cd $DIR_IDE/../Espruino
 source ../emsdk/emsdk_env.sh
-make clean;RELEASE=1 BOARD=EMSCRIPTEN make
+make clean;RELEASE=1 BOARD=EMSCRIPTEN make || exit 1
+make clean;RELEASE=1 BOARD=EMSCRIPTEN2 make || exit 1
 cd $DIR_IDE
-cp $DIR_IDE/../Espruino/emulator_espruino.js js/plugins/emulator_espruino.js
+cp $DIR_IDE/../Espruino/emulator_banglejs*.js js/emu
 echo Finished.
